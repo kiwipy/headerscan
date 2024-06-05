44a151d18b716fd29bbe37a12da3c8bdfb4f03a9
William Andersson <william.andersson@outlook.com>
2024-06-05 13:32:09 +0200

    Add files via upload

--- headerscan.sh
+++ headerscan.sh
@@ -6,16 +6,37 @@
 # Website:     https://github.com/william-andersson
 # License:     GPL
 #
-VERSION=0.1.0
+VERSION=0.2.0
 
-echo "------------ HeaderScan v$VERSION ------------"
+echo -e "\n--------------- HeaderScan v$VERSION ---------------"
 OUT="$(mktemp /tmp/ipinfo.XXXX)"
 PARSED="$(mktemp /tmp/head-scan.XXXX)"
 
+wait_graphics(){
+    graphics=(
+        "( ●    )"
+        "(  ●   )"
+        "(   ●  )"
+        "(    ● )"
+        "(     ●)"
+        "(    ● )"
+        "(   ●  )"
+        "(  ●   )"
+        "( ●    )"
+        "(●     )"
+    )
+    i=0
+    while kill -0 $pid 2>/dev/null;
+    do
+        i=$(( (i+1) %10 ))
+        echo -ne "\r${graphics[$i]}"
+        sleep .1
+    done
+    echo -ne "\r"
+}
+
 parse_input_file(){
-    #
     # Update keyword_list
-    #
     for a in $(grep -i "^[A-Z]" $1 | awk '{print $1}');do
         if [[ "$a" =~ [A-Z] ]];then
             b=${a%?}':'
@@ -29,9 +50,7 @@ parse_input_file(){
         fi
     done
 
-    #
     # Parse to file
-    #
     for i in $(cat $1);do
         y="0"
         for x in $(cat /usr/local/share/headerscan/keyword_list);do
@@ -55,9 +74,7 @@ parse_input_file(){
 }
 
 get_ip_info(){
-    #
-    # Collect info about ip
-    #
+    # Collect info about ip-address
     curl -s https://ipinfo.io/$1 > $OUT
 
     IP="$(cat $OUT | grep "\"ip\":" | cut -f4- -d ' ' | sed 's/\"//g' | sed 's/\,//g')"
@@ -69,67 +86,79 @@ get_ip_info(){
 
     if [ "$2" == "red" ];then
         if [ "$BOGON" == "true" ];then
-            echo -e "\033[33m  IP: Bogon address reserved for special use.\033[0m"
+            echo -e "\033[33m  |    IP: Bogon address reserved for special use.\033[0m"
         else
-            echo -e "\033[31m  IP: $IP\n  Location: $CITY, $COUNTRY\n  Timezone: $TIME\n  Provider: $PROVIDER\033[0m"
+            echo -e "\033[31m  |    IP: $IP\n  |    Location: $CITY, $COUNTRY\n  |    Timezone: $TIME\n  |    Provider: $PROVIDER\033[0m"
         fi
     else
-        echo -e "  Location: $CITY, $COUNTRY\n  Timezone: $TIME\n  Provider: $PROVIDER"
+        echo -e "  |    Location: $CITY, $COUNTRY\n  |    Timezone: $TIME\n  |    Provider: $PROVIDER"
     fi
 }
 
-parse_input_file $1
+parse_input_file $1 &
+pid=$!
+wait_graphics
+
 # Print email main info
-echo "$(cat $PARSED | awk '/^Date: /')"
-echo "$(cat $PARSED | awk '/^From: /')"
-echo "$(cat $PARSED | awk '/^To: /')"
-echo "$(cat $PARSED | awk '/^Subject: /')"
-echo ""
-echo "$(cat $PARSED | awk '/^Return-Path: / {print $1, $2}')"
-echo "$(cat $PARSED | awk '/^Message-ID: /')"
-echo "$(cat $PARSED | awk '/^MIME-Version: /')"
-echo ""
+echo "[General info]"
+echo "  $(cat $PARSED | awk '/^Date: /')"
+echo "  $(cat $PARSED | awk '/^From: /')"
+echo "  $(cat $PARSED | awk '/^To: /')"
+echo "  $(cat $PARSED | awk '/^Subject: /')"
+echo -e "\n[Other info]"
+echo "  $(cat $PARSED | awk '/^MIME-Version: /')"
+echo "  $(cat $PARSED | awk '/^Return-Path: / {print $1, $2}')"
+echo "  $(cat $PARSED | awk '/^Message-ID: /')"
 
 # Print Authentication-Results
-echo "SPF: $(cat $PARSED | grep -o '[^ ]*spf=[^ ]*' | cut -d "=" -f2)"
-echo "DKIM: $(cat $PARSED | grep -o '[^ ]*dkim=[^ ]*' | cut -d "=" -f2)"
-echo "DMARC: $(cat $PARSED | grep -o '[^ ]*dmarc=[^ ]*' | cut -d ";" -f2 | cut -d "=" -f2)"
-echo ""
+echo -e "\n[Authentication]"
+echo "  SPF: $(cat $PARSED | grep -o '[^ ]*spf=[^ ]*' | cut -d "=" -f2)"
+echo "  DKIM: $(cat $PARSED | grep -o '[^ ]*dkim=[^ ]*' | cut -d "=" -f2)"
+echo "  DMARC: $(cat $PARSED | grep -o '[^ ]*dmarc=[^ ]*' | cut -d ";" -f2 | cut -d "=" -f2)"
+
+# Print helo strings
+echo -e "\n[HELO strings]"
 for helo in $(cat $PARSED | grep -o '[^ ]*helo=[^ ]*' | cut -d "=" -f2 | sed 's/.\{1\}$//');do
-    echo "HELO: $helo"
+    echo "  HELO: $helo"
 done
-echo ""
 
 # Print Received-SPF info
+echo -e "\n[Received-SPF]"
 SPF_DOM=$(cat $PARSED | awk '/^Received-SPF: / {print $6}')
 SPF_IP=$(cat $PARSED | awk '/^Received-SPF: / {print $8}')
-echo -e "Received-SPF: \033[34m$SPF_DOM\033[0m ($SPF_IP)"
-
+echo -e "  Received-SPF: \033[34m$SPF_DOM\033[0m ($SPF_IP)"
 
 # Print all Received: fields
+echo -e "\n[Email path through network]"
+echo "  |--(Receiver)"
 for i in $(cat $PARSED | awk '/Received: from/ {print $3}' | sed 's/\[//g' | sed 's/\]//g');do
     # If $i is a IP, print waring and look-up
     if [[ $i =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-        echo -e "\033[31mReceived: Source without URL --> $i\033[0m"
+        echo -e "\033[31m  |  Received: Source without URL --> $i\033[0m"
         get_ip_info $i red
     else
         # Print ip after every host if exists
         HOST_IP=$(host $i | awk 'NR==1{print $4}')
         if [ "$HOST_IP" == "$SPF_IP" ];then
-            echo -e "Received: \033[34m$i\033[0m $(host $i | awk 'NR==1{print $3, $4}')"
+            echo -e "  |  Received: \033[34m$i\033[0m $(host $i | awk 'NR==1{print $3, $4}')"
         else
-            echo "Received: $i $(host $i | awk 'NR==1{print $3, $4}')"
+            echo "  |  Received: $i $(host $i | awk 'NR==1{print $3, $4}')"
         fi
         if [ "$HOST_IP" != "found:" ];then
             get_ip_info $HOST_IP
         fi
     fi
 done
+echo "  |--(Sender)"
 
 echo ""
 read -e -p "Save parsed file to: " DEST
 if [ ! -z $DEST ];then
-    cp $PARSED ${DEST%/}/header-$(date +%d-%m-%Y_%H:%M)
+    if [ -d $DEST ];then
+        cp $PARSED ${DEST%/}/header-$(date +%d-%m-%Y_%H:%M)
+    else
+        echo "No such directory!"
+    fi
 fi
 rm $OUT
 rm $PARSED
