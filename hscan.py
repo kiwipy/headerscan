247c95f634599c7acf7747dfcdd22b71467fe8ad
William Andersson <william.andersson@outlook.com>
2024-06-12 13:37:58 +0200

    v1.0.2
    Code cleanup and pep8 checked

--- hscan.py
+++ hscan.py
@@ -18,11 +18,11 @@ if sys.version_info < (3, 6):
     sys.exit(1)
 
 base64 = "no"
-MARKS = 0
-GENERAL = []
-OTHER = []
-HELO = []
-RECEIVED = []
+marks = 0
+general = []
+other = []
+helo = []
+received = []
 RED = '\033[31m'
 RED_BOLD = '\033[31m\033[1m'
 BLUE_BOLD = '\033[34m\033[1m'
@@ -44,6 +44,7 @@ else:
     print("Usage: hscan </path/to/header-file>")
     sys.exit(1)
 
+
 def parse_file():
     # Parse input file, extract and decode base64.
     parsed_file = open("/tmp/hscan/parsed", "w")
@@ -51,6 +52,7 @@ def parse_file():
     parsed_stat = "null"
     base64_stat = "null"
     global base64
+
     with open(input_file, "r") as subject:
         parsed_file.write(f"----- Parse file for HeaderScan v{VERSION} -----")
         for line in subject:
@@ -79,10 +81,11 @@ def parse_file():
                 elif line.startswith("--"):
                     base64_stat = "skip"
                 if base64_stat == "write" and not \
-                    line.startswith("Content-Transfer-Encoding: base64"):
+                       line.startswith("Content-Transfer-Encoding: base64"):
                     base64_file.write(f"{line}")
                 else:
                     pass
+
     base64_file.close()
     if base64 == "yes":
         os.system("base64 -w0 -d /tmp/hscan/base64 > /tmp/hscan/text64")
@@ -90,182 +93,186 @@ def parse_file():
     
 def collect_data():
     # Collect data from parsed file.
-    global GENERAL, OTHER, FROM_ADDR, REPLY_ADDR, SPF, \
-           DKIM, DMARC, HELO, SPF_DOMAIN, SPF_IP, RECEIVED
+    global general, other, from_addr, reply_addr, spf, \
+           dkim, dmarc, helo, spf_domain, spf_ip, received
+
     with open("/tmp/hscan/parsed", "r") as subject:
         for line in subject:
             words = line.split()
             if len(words) > 0 and words[0] != "-----":
                 if words[0] in ["Date:", "From:", "To:", "Subject:"]:
-                    GENERAL.append(line.rstrip("\n"))
+                    general.append(line.rstrip("\n"))
                     if words[0] == "From:":
-                        FROM_ADDR = words[-1].strip("<").rstrip(">")
+                        from_addr = words[-1].strip("<").rstrip(">")
                 elif words[0] in ["MIME-Version:", "Return-Path:", 
                                   "Reply-To:", "Message-ID:"]:
-                    OTHER.append(line.rstrip("\n"))
+                    other.append(line.rstrip("\n"))
                     if words[0] == "Reply-To:":
-                        REPLY_ADDR = words[-1].strip("<").rstrip(">")
+                        reply_addr = words[-1].strip("<").rstrip(">")
                 elif words[0] == "Authentication-Results:":
                     for word in words:
                         if "spf=" in word:
-                            SPF = word.split("=")[-1]
+                            spf = word.split("=")[-1]
                         elif "dkim=" in word:
-                            DKIM = word.split("=")[-1]
+                            dkim = word.split("=")[-1]
                         elif "dmarc=" in word:
-                            DMARC = word.split("=")[-1]
+                            dmarc = word.split("=")[-1]
                 elif words[0] == "Received-SPF:":
-                    SPF_TMP = words[5].split(".")[-2:]
-                    SPF_DOMAIN = (f"{SPF_TMP[0]}.{SPF_TMP[1]}")
-                    SPF_IP = words[7]
+                    spf_tmp = words[5].split(".")[-2:]
+                    spf_domain = (f"{spf_tmp[0]}.{spf_tmp[1]}")
+                    spf_ip = words[7]
                     for word in words:
                         if "helo=" in word:
-                            HELO.append(word.split("=")[-1].rstrip(";"))
+                            helo.append(word.split("=")[-1].rstrip(";"))
                 elif words[0] == "Received:":
                     for word in words:
                         if "helo=" in word:
-                            HELO.append(word.split("=")[-1].rstrip(")"))
-                    RECEIVED.append(words)
+                            helo.append(word.split("=")[-1].rstrip(")"))
+                    received.append(words)
 
 
-def get_ip_info(IP, COLOR):
+def get_ip_info(ip, color):
     # Parse and print info for IP address
-    os.system(f"curl -s https://ipinfo.io/{IP} > /tmp/hscan/ipinfo")
-    OUTPUT = {}
+    os.system(f"curl -s https://ipinfo.io/{ip} > /tmp/hscan/ipinfo")
+    output = {}
+
     with open("/tmp/hscan/ipinfo", "r") as subject:
         for line in subject:
             if ":" in line:
                 for words in line.split(",  "):
                     word = words.split(": ")
-                    KEY = word[0].strip(" \"").rstrip("\"")
-                    VAL = word[1].strip(" \"").rstrip(",\n\"")
-                    OUTPUT[KEY] = VAL
-    if "bogon" in OUTPUT:
-        if OUTPUT['bogon'] == 'true':
+                    add_key = word[0].strip(" \"").rstrip("\"")
+                    add_val = word[1].strip(" \"").rstrip(",\n\"")
+                    output[add_key] = add_val
+
+    if "bogon" in output:
+        if output['bogon'] == 'true':
             print(f"  |                  {ORANGE_BOLD}IP: " \
                   f"Bogon address reserved for special use.{END}")
     else:
-        for key, value in OUTPUT.items():
-            if COLOR == "red":
+        for key, value in output.items():
+            if color == "red":
                 if key in ["ip", "city", "country", "org", "timezone"]:
                     if key == "org":
-                        print(f"  |                  " \
-                              f"{RED}{key} = {value.partition(' ')[2]}{END}")
+                        print(f"  |                  {RED}" \
+                              f"{key} = {value.partition(' ')[2]}{END}")
                     else:
-                        print(f"  |                  " \
-                              f"{RED}{key} = {value}{END}")
+                        print(f"  |                  {RED}" \
+                              f"{key} = {value}{END}")
             else:
                 if key in ["city", "country", "org", "timezone"]:
                     if key == "org":
-                        print(f"  |                  " \
-                              f"{key} = {value.partition(' ')[2]}")
+                        print(f"  |                  {key}" \
+                              f" = {value.partition(' ')[2]}")
                     else:
                         print(f"  |                  {key} = {value}")
         
 
 def print_summery():
-    global MARKS
+    global marks
     TOT_MARKS = 9
-    VERDICT = []
+    verdict = []
+
     print("[General info]")
-    for item in GENERAL:
+    for item in general:
         print(f"  {item}")
     print("\n[Other info]")
-    for item in OTHER:
+    for item in other:
         print(f"  {item}")
 
     print("\n[Authentication]")
-    if SPF != "pass":
-        print(f"  SPF: {RED_BOLD}{SPF}{END}")
-        VERDICT.append("  - SPF not passed.")
-        MARKS += 1
+    if spf != "pass":
+        print(f"  SPF: {RED_BOLD}{spf}{END}")
+        verdict.append("  - SPF not passed.")
+        marks += 1
     else:
-        print(f"  SPF: {SPF}")
-    if DKIM != "pass":
-        print(f"  DKIM: {RED_BOLD}{DKIM}{END}")
-        VERDICT.append("  - DKIM not passed.")
-        MARKS += 1
+        print(f"  SPF: {spf}")
+    if dkim != "pass":
+        print(f"  DKIM: {RED_BOLD}{dkim}{END}")
+        verdict.append("  - DKIM not passed.")
+        marks += 1
     else:
-        print(f"  DKIM: {DKIM}")
-    if DMARC != "pass":
-        print(f"  DMARC: {RED_BOLD}{DMARC}{END}")
-        VERDICT.append("  - DMARC not passed.")
-        MARKS += 1
+        print(f"  DKIM: {dkim}")
+    if dmarc != "pass":
+        print(f"  DMARC: {RED_BOLD}{dmarc}{END}")
+        verdict.append("  - DMARC not passed.")
+        marks += 1
     else:
-        print(f"  DMARC: {DMARC}")
+        print(f"  DMARC: {dmarc}")
     
     print("\n[HELO strings]")
-    for addr in HELO:
+    for addr in helo:
         if "." in addr:
             print(f"  HELO: {BLUE_BOLD}{addr}{END}")
         else:
             print(f"  HELO: {RED_BOLD}{addr}{END}")
-            VERDICT.append("  - HELO string without domain.")
-            MARKS += 1
+            verdict.append("  - HELO string without domain.")
+            marks += 1
     
     print("\n[Received-SPF]")
-    print(f"  Received-SPF: {BLUE_BOLD}{SPF_DOMAIN}{END} ({SPF_IP})")
+    print(f"  Received-SPF: {BLUE_BOLD}{spf_domain}{END} ({spf_ip})")
     
     print("\n[Email path through network]")
     print("  |-(Receiver)")
-    for id in RECEIVED:
+    for id in received:
         if id[1] != "by":
-            IP = id[3].strip("(").rstrip(")")
-            IFIP = id[2].strip("[").rstrip("]")
-            if re.match(r'^(\d{1,3}\.){3}\d{1,3}$', IFIP):
+            ip = id[3].strip("(").rstrip(")")
+            if_ip = id[2].strip("[").rstrip("]")
+            if re.match(r'^(\d{1,3}\.){3}\d{1,3}$', if_ip):
                 # If domain name is IP address.
                 print(f"  |  {RED_BOLD}{id[0]} {id[1]}: " \
-                      f"Source without URL --> {IFIP}{END}")
-                get_ip_info(IFIP, "red")
-                VERDICT.append("  - Received from without domain name.")
-                MARKS += 1
-            elif re.match(r'^(\d{1,3}\.){3}\d{1,3}$', IP):
+                      f"Source without URL --> {if_ip}{END}")
+                get_ip_info(if_ip, "red")
+                verdict.append("  - Received from without domain name.")
+                marks += 1
+            elif re.match(r'^(\d{1,3}\.){3}\d{1,3}$', ip):
                 # If IPV4 Look up domain IP.
-                if IP == SPF_IP:
-                    if SPF_DOMAIN in id[2]:
-                        if id[2] not in HELO:
-                            VERDICT.append("  - Received from domain " \
+                if ip == spf_ip:
+                    if spf_domain in id[2]:
+                        if id[2] not in helo:
+                            verdict.append("  - Received from domain " \
                                            "not in HELO.")
                             print(f"  |  {id[0]} {id[1]}: " \
                                   f"{RED_BOLD}{id[2]}{END} {id[3]}")
-                            MARKS += 1
+                            marks += 1
                         else:
                             print(f"  |  {id[0]} {id[1]}: " \
                                   f"{BLUE_BOLD}{id[2]}{END} {id[3]}")
                     else:
-                        VERDICT.append("  - Received from not same " \
+                        verdict.append("  - Received from not same " \
                                        "domain as SPF.")
                         print(f"  |  {id[0]} {id[1]}: " \
                               f"{RED_BOLD}{id[2]}{END} {id[3]}")
-                        MARKS += 1
+                        marks += 1
                 print(f"  |              {id[4]}: {id[5]}")
-                get_ip_info(IP, "none")
+                get_ip_info(ip, "none")
             elif "localhost" in id[2]:
                 # If domain name = localhost
                 print(f"  |  {ORANGE_BOLD}{id[0]} {id[1]}: " \
                       f"Source without URL --> {id[2]}{END}")
-                VERDICT.append("  - Received from localhost.")
-                MARKS += 1
+                verdict.append("  - Received from localhost.")
+                marks += 1
             else:
                 print(f"  |  {id[0]} {id[1]}: {id[2]}")
                 print(f"  |              {id[4]}: {id[5]}")
     print("  |-(Sender)")
 
-    if 'REPLY_ADDR' in globals():
-        if REPLY_ADDR != FROM_ADDR:
-            MARKS += 1
-            VERDICT.append("  - REPLY address not same as FROM.")
+    if 'reply_addr' in globals():
+        if reply_addr != from_addr:
+            marks += 1
+            verdict.append("  - REPLY address not same as FROM.")
     
     print("\n[Verdict]")
-    if MARKS < 2:
+    if marks < 2:
         print(f"  {BLUE_BOLD}Number of suspect features: " \
-              f"{MARKS}/{TOT_MARKS}{END}")
-    elif MARKS < int(TOT_MARKS/2):
+              f"{marks}/{TOT_MARKS}{END}")
+    elif marks < int(TOT_MARKS/2):
         print(f"  {ORANGE_BOLD}Number of suspect features: " \
-              f"{MARKS}/{TOT_MARKS}{END}")
+              f"{marks}/{TOT_MARKS}{END}")
     else:
         print(f"  {RED_BOLD}Number of suspect features: " \
-              f"{MARKS}/{TOT_MARKS}{END}")
-    for note in VERDICT:
+              f"{marks}/{TOT_MARKS}{END}")
+    for note in verdict:
         print(note)
 
   
