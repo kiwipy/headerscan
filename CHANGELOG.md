3dae71b6ba14c3beb2f9889ec37c6dd696fb6a94
William Andersson <william.andersson@outlook.com>
2025-01-27 16:02:24 +0100

    changelog + minor fix

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+## [Unreleased]
+
+## [1.0.3] - 2024-08-30
+### Fixed
+- List out of range if multiple Received-SPF fields
+
+## [1.0.2] - 2024-06-29
+### Fixed
+- Check if python version is 3.6 or higher.
+- Code cleanup
+
+## [1.0.1] - 2024-06-11
+### Fixed
+- REPLY address not same as FROM. even if it is.
+
+## [1.0.0] - 2024-06-11
+_First release_
--- install.sh
+++ install.sh
@@ -3,7 +3,12 @@ if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root"
     exit 1
 fi
-echo "*** Installing HeaderScan $(grep 'VERSION=' hscan.py | sed 's/^.*=//') ***"
+
+VERSION=$(grep 'VERSION=' hscan.py | sed 's/^.*=//')
+if [[ $VERSION == *"\""* ]];then
+    VERSION=${VERSION:1:-1}
+fi
+echo "*** Installing HeaderScan v$VERSION ***"
 
 # Program files (always install)
 install -C -D -m 755 -v hscan.py /usr/local/bin/hscan
